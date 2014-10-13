8d8e584e07e0963e101664bcbd3536e4929a2c57
Mark Fickett <mark.fickett@gmail.com>
2014-10-13 12:18:18 -0400

    add full cold restart

--- gpslogger.ino
+++ gpslogger.ino
@@ -1,6 +1,7 @@
 // Example from https://www.sparkfun.com/tutorials/176
 void setup() {
   Serial.begin(57600);
+  Serial.println("$PMTK104*37"); // FULL COLD RESTART
 }
 void loop() {
   if (Serial.available()) {
